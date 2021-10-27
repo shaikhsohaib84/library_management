20db387304a0643296045edd758e927ef90555c6
sohaib <sohaib.shaikh@livehealth.in>
2021-10-27 17:58:18 +0530

    intial model created

---
+++ library/crud/choices.py
@@ -0,0 +1,6 @@
+BOOK_TYPE_CHOICES = (
+    (1, ("Novel")),
+    (2, ("Sc-fci")),
+    (3, ("Drama")),
+    (4, ("Autobiography"))
+)
---
+++ library/crud/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+from crud.choices import BOOK_TYPE_CHOICES
+# Create your models here.
+
+'''
+from passlib.hash import pbkdf2_sha256
+hash = pbkdf2_sha256.hash("toomanysecrets")
+'''
+class Admin(models.Model):
+    id = models.AutoField(blank=False, null=False, primary_key=True)
+    name = models.CharField(max_length=20, blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False, unique=True)
+    password = models.TextField(max_length=255, blank=False, null=False) 
+
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        db_table = 'Admin'
+
+    def __str__(self):
+        return self.email
+    
+
+class Book(models.Model):
+    id = models.AutoField(blank=False, null=False, primary_key=True)
+    book_name = models.CharField(max_length=50, blank=False, null=False)
+    book_author_name = models.CharField(max_length=50, blank=False, null=False)
+    book_price = models.IntegerField(blank=False, null=False)
+    book_type = models.CharField(max_length=50, blank=False, null=False, default=1, choices=BOOK_TYPE_CHOICES)
+
+    class Meta:
+        db_table = 'Book'
+
+    def __str__(self):
+        return self.id
